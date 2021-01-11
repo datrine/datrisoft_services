7c86fa5a58f6386c3cd470fbb7294bf6f51f1bed
datrine <trinitietp@gmail.com>
2021-01-11 16:29:34 +0100

    first commit

--- index.js
+++ index.js
@@ -3,32 +3,26 @@ let app = express()
 let cors = require("cors")
 app.use(express.json())
 let emailRouter = require("./services/email")
+app.use(cors())
 
-var whitelist = ['http://localhost:3000', 'http://example2.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions))
-
-app.use("/", (req, res, next) => {
-    res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>"); 7
-    next()
-})
-app.post("/testing",cors(corsOptions), (req, res, next) => {
+app.post("/testing/", (req, res, next) => {
     res.json({ send: true })
 })
-app.get("/testing",cors(corsOptions), (req, res, next) => {
+app.get("/testing/", (req, res, next) => {
     res.json({ send: true })
 })
 app.use("/email/", emailRouter)
-console.log("Loaded..")
+
+app.post("/", (req, res, next) => {
+    res.json({ served: "ygyugyg" });
+    next()
+})
+
+app.get("/", (req, res, next) => {
+    res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>");
+    next()
+})
+console.log("Loaded...")
 app.listen(5000, () => {
     console.log("Listening...")
 })
