14a21b35cd03b3d5657d05636a93259d8315337b
datrine <trinitietp@gmail.com>
2021-01-11 12:43:24 +0100

    first commit

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use("/", (req, res, next) => {
     res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>"); 7
     next()
 })
-app.post("/testing", (req, res, next) => {
+app.post("/testing",cors(corsOptions), (req, res, next) => {
     res.json({ send: true })
 })
-app.get("/testing", (req, res, next) => {
+app.get("/testing",cors(corsOptions), (req, res, next) => {
     res.json({ send: true })
 })
 app.use("/email/", emailRouter)
