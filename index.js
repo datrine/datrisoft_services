3f2d299169e208d4388f6c433f059c33c8959b99
datrine <trinitietp@gmail.com>
2021-01-11 12:00:36 +0100

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.use(express.json())
 let emailRouter = require("./services/email")
 
 app.use("/", (req, res, next) => {
-    res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>");
+    res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>");7
+    next()
+})
+app.post("/testing",(req, res, next)=>{
+res.json({send:true})
 })
 app.use("/email/", emailRouter)
 console.log("Loaded..")
