a2ba496249fa5f9111bc1c3e602d6b4fcc1b0a17
datrine <trinitietp@gmail.com>
2021-01-11 12:38:08 +0100

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 let express = require("express");
 let app = express()
 let cors = require("cors")
-app.options('*', cors())
 app.use(express.json())
 let emailRouter = require("./services/email")
+
+var whitelist = ['http://localhost:3000', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 app.use("/", (req, res, next) => {
     res.send("<p>Services for <a href='https://www.datrisoft.com'>Datrisoft</a>...</p>"); 7
