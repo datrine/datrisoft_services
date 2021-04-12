b365d38e997001db5edcb96e1c709b17f1e96194
datrine <trinitietp@gmail.com>
2021-04-12 21:19:50 +0100

    make it better

--- services/chat.js
+++ services/chat.js
@@ -1,7 +1,9 @@
-const { Server,Socket } = require("socket.io")
+const { Server, Socket } = require("socket.io")
 function startSocket(server) {
-const {port:PORT} = server.address()
-console.log("Socket io port is..." + PORT)
+    if (process.env.NODE_ENV !== "production") {
+        const { port: PORT } = server.address()
+        console.log("Socket io port is..." + PORT)
+    }
     const io = new Server(server, {
         cors: {
             origin: "*",
@@ -10,18 +12,18 @@ console.log("Socket io port is..." + PORT)
         }
     });
 
-    io.on("connection", 
-    /**
-     * @param {Socket} socket
-     */
-    (socket) => {
-        console.log("Connected...")
-        console.log(socket.handshake.query.serviceId)
-        socket.on("hello",({sender})=>{
-            console.log(sender)
-        })
-        io.emit("welcome","client")
-    });
+    io.on("connection",
+        /**
+         * @param {Socket} socket
+         */
+        (socket) => {
+            console.log("Connected...")
+            console.log(socket.handshake.query.serviceId)
+            socket.on("hello", ({ sender }) => {
+                console.log(sender)
+            })
+            io.emit("welcome", "client")
+        });
 
 }
 module.exports = { startSocket }
