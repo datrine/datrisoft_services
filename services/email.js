4cab1c0993da07f083c3e77eb8513b8246bbdac2
datrine <trinitietp@gmail.com>
2021-06-17 11:55:47 +0100

    email services

--- services/email.js
+++ services/email.js
@@ -5,6 +5,7 @@ router.post("/send/", (req, res, next) => {
     if (!req.body) {
         return;
     }
+    console.log("Email host: "+req.body.host);
     let { from, to, text, html, pass, user, host, port, subject } = req.body;
     let transport = nodemailer.createTransport({
         host, port, auth: { user, pass },
